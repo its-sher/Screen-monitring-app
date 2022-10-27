a2f41a51e63dd78bf8c62ece3904f2eff76ab1f8
unknown <devajivainfotech@gmail.com>
2022-10-27 23:01:28 +0530

    Akshay Thakur 27/10/2022
    index.html
    Login.html
    script.js
    session.js
    style.js
    Login.js

--- client/app/script.js
+++ client/app/script.js
@@ -1,64 +1,67 @@
-var attempt = 3; // Variable to count number of attempts.
+// var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
-function validate(){
-var username = document.getElementById("logName").value;
-var password = document.getElementById("logPassword").value;
-if ( username == "akshay" && password == "akshay"){
-window.location = "index.html"; // Redirecting to other page.
-return false;
+function validate() {
+  var email = document.getElementById("logName").value;
+  // console.log(email);
+  var password = document.getElementById("logPassword").value;
+  //console.log(password);
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json; charset=UTF-8");
+  //myHeaders.append("access_token", "MTNlZTU4NGUtYjE3MC00MmMyLWIwYWMtN2Y0ZTU5OWEwMTE2MmRkZDk5ZmE0NGU1NjhiNGI4MmVmM2MzZjNiZTJmMjI=");
+  //myHeaders.append("refresh_token", "2ddd99fa44e568b4b82ef3c3f3be2f22");
+
+  var raw = {};
+  raw["email"] = email;
+  raw["password"] = password;
+  //console.log(raw);
+  const stringifiedData = JSON.stringify(raw);
+
+  var requestOptions = {
+    method: "Post",
+    headers: myHeaders,
+    body: stringifiedData,
+    redirect: "follow",
+  };
+  //console.log(requestOptions);
+  fetch("http://localhost:8000/login", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  //-------------------------------------------End-------------------------------------------//
+  // if ( email == email && password == password){
+  // window.location = "index.html"; // Redirecting to other page.
+  // return false;
+  // }
+  // else{
+
+  //   blankField.textContent = "Please check your Username and Password"
+  // // attempt --;// Decrementing by one.
+  // // alert("You have left "+attempt+" attempt;");
+  // // Disabling fields after 3 attempts.
+  // // if( attempt == 0){
+  // // document.getElementById("logName").disabled = true;
+  // // document.getElementById("logPassword").disabled = true;
+  // // document.getElementById("submit").disabled = true;
+  // // return false;
+  // // }
+  //   // emailerror2.textContent = "Please enter User and Password"
+  // }
 }
-else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("logName").disabled = true;
-document.getElementById("logPassword").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
-}
-  function hello(){
-    window.location.href = "login.html"; // Redirecting to other page.
-    return false; 
-  }
-//----------------------------------------End-------------------------------------//   
-
-//--------------------------Post------------------Start-----------------//
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json; charset=UTF-8");
-myHeaders.append("access_token", "MTNlZTU4NGUtYjE3MC00MmMyLWIwYWMtN2Y0ZTU5OWEwMTE2MmRkZDk5ZmE0NGU1NjhiNGI4MmVmM2MzZjNiZTJmMjI=");
-myHeaders.append("refresh_token", "2ddd99fa44e568b4b82ef3c3f3be2f22");
-
-var raw = "{\r\n    \"email\": \"abc@gmail.com\",\r\n    \"password\": \"password1@\"\r\n}";
-// \r\n\:Used as a new line character in Windows
-
-var requestOptions = {
-  method: 'Post',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:8000/login", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-//-------------------------------------------End-------------------------------------------//
+//----------------------------------------End-------------------------------------//
 
 //---------for refresh-------------//
-function refreshPage(){
+function refreshPage() {
   window.location.reload();
-} 
+}
 
 //---------------for timer--------------//
-function Timer(){
-var btn = document.getElementById("btn").value;
-{
-window.location.href = "Timer.html"; // Redirecting to other page.
-}};
+function Timer() {
+  var btn = document.getElementById("btn").value;
+  {
+    window.location.href = "Timer.html"; // Redirecting to other page.
+  }
+}
 // function update() {
 // 	var select = document.getElementById('input');
 // 	var option = select.options[select.selectedIndex];
@@ -67,19 +70,23 @@ window.location.href = "Timer.html"; // Redirecting to other page.
 // update();
 
 //--------for add task field --------//
-  function plus_icon() {
-    document.querySelector(".AddTaskInput").style.display = "block";
-    document.querySelector("#input").style.display = "none";
-    document.querySelector(".plus_icon").style.display = "none";
-  }
+function plus_icon() {
+  document.querySelector(".AddTaskInput").style.display = "block";
+  document.querySelector("#input").style.display = "none";
+  document.querySelector(".plus_icon").style.display = "none";
+}
 
-  function AddTaskCancel() {
-    document.querySelector(".AddTaskInput").style.display = "none";
-    document.querySelector("#input").style.display = "block";
-    document.querySelector(".plus_icon").style.display = "block";
-  }
+function AddTaskCancel() {
+  document.querySelector(".AddTaskInput").style.display = "none";
+  document.querySelector("#input").style.display = "block";
+  document.querySelector(".plus_icon").style.display = "block";
+}
 
-  function AddTaskInput() {
-    document.querySelector(".AddTaskSave").style.display = "block";
-  }
- 
+function AddTaskInput() {
+  document.querySelector(".AddTaskSave").style.display = "block";
+}
+
+function hello() {
+  window.location.href = "login.html"; // Redirecting to other page.
+  return false;
+}
--- client/app/session.js
+++ client/app/session.js
@@ -1,14 +1,37 @@
-function store() { //stores items in the sessionStorage
-    var email = document.getElementById('logName').value;
-    var password = document.getElementById('logPassword').value;
-  
-    const login = {
-        email: email,
-        password: password,
-    }
-    window.sessionStorage.setItem('login',JSON.stringify(login));  
-  }
+function store() {
+  //stores items in the sessionStorage
+  var email = document.getElementById("logName").value;
+  var password = document.getElementById("logPassword").value;
 
-  window.onload =function() { //ensures the page is loaded before functions are executed.
-        document.getElementById("Login_form").onsubmit = store;
-  }
+  const login = {
+    email: email,
+    password: password,
+  };
+  window.sessionStorage.setItem("login", JSON.stringify(login));
+}
+//   function removeItem() {//deletes item from sessionStorage
+//     sessionStorage.removeItem('Login_form');
+//     console.log("login");
+// }
+
+window.onload = function () {
+  //ensures the page is loaded before functions are executed.
+  document.getElementById("Login_form").onclick = store;
+  //document.getElementById("removeButton").onclick = removeItem;
+};
+
+// Get the text field that we're going to track
+// let field = document.getElementById("logName");
+// if (sessionStorage.getItem("email")) {
+//   field.value = sessionStorage.getItem("email");
+// }
+// field.addEventListener("change", () => {
+//   sessionStorage.setItem("email", field.value);
+// });
+// let field1 = document.getElementById("logPassword");
+// if (sessionStorage.getItem("password")) {
+//   field1.value = sessionStorage.getItem("password");
+// }
+// field1.addEventListener("change", () => {
+//   sessionStorage.setItem("password", field1.value);
+// });
