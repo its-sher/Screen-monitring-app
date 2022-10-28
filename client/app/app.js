23e2484cfac4d54153c85ef2327749f70bc50507
unknown <devajivainfotech@gmail.com>
2022-10-28 22:53:11 +0530

    Akshay Thakur 28/10/2022
    Re-install Modules files .
    script.js---->>> form validation
    login.html---->> Login with dynamic data
    session.js-->>remove scripts and inserted into script.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,11 @@
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-	let mainWindow = new BrowserWindow({
-		width: 500,
-		height: 640,
-		icon:'app/icon.png',
-	  });
-	mainWindow.loadFile('./app/Login.html');
-	mainWindow.setResizable(false);
+const { app, BrowserWindow } = require("electron");
+function createWindow() {
+  let mainWindow = new BrowserWindow({
+    width: 500,
+    height: 640,
+    icon: "app/icon.png",
+  });
+  mainWindow.loadFile("./app/Login.html");
+  //   mainWindow.setResizable(true);
 }
 app.whenReady().then(createWindow);
--- client/app/script.js
+++ client/app/script.js
@@ -2,51 +2,35 @@
 // Below function Executes on click of login button.
 function validate() {
   var email = document.getElementById("logName").value;
-  // console.log(email);
   var password = document.getElementById("logPassword").value;
-  //console.log(password);
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json; charset=UTF-8");
-  //myHeaders.append("access_token", "MTNlZTU4NGUtYjE3MC00MmMyLWIwYWMtN2Y0ZTU5OWEwMTE2MmRkZDk5ZmE0NGU1NjhiNGI4MmVmM2MzZjNiZTJmMjI=");
-  //myHeaders.append("refresh_token", "2ddd99fa44e568b4b82ef3c3f3be2f22");
-
   var raw = {};
   raw["email"] = email;
   raw["password"] = password;
-  //console.log(raw);
   const stringifiedData = JSON.stringify(raw);
-
   var requestOptions = {
     method: "Post",
     headers: myHeaders,
     body: stringifiedData,
     redirect: "follow",
   };
-  //console.log(requestOptions);
   fetch("http://localhost:8000/login", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-
-  //-------------------------------------------End-------------------------------------------//
-  // if ( email == email && password == password){
-  // window.location = "index.html"; // Redirecting to other page.
-  // return false;
-  // }
-  // else{
-
-  //   blankField.textContent = "Please check your Username and Password"
-  // // attempt --;// Decrementing by one.
-  // // alert("You have left "+attempt+" attempt;");
-  // // Disabling fields after 3 attempts.
-  // // if( attempt == 0){
-  // // document.getElementById("logName").disabled = true;
-  // // document.getElementById("logPassword").disabled = true;
-  // // document.getElementById("submit").disabled = true;
-  // // return false;
-  // // }
-  //   // emailerror2.textContent = "Please enter User and Password"
-  // }
+    .then((result) => {
+      if (result.status == "200") {
+        const login = {
+          email: email,
+          password: password,
+        };
+        window.sessionStorage.setItem("login", JSON.stringify(login));
+        window.location = "index.html";
+      } else {
+        blankField.textContent = "Please Enter Correct Credentials";
+      }
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
 }
 //----------------------------------------End-------------------------------------//
 
@@ -55,6 +39,14 @@ function refreshPage() {
   window.location.reload();
 }
 
+function showpass() {
+  var x = document.getElementById("logPassword");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 //---------------for timer--------------//
 function Timer() {
   var btn = document.getElementById("btn").value;
@@ -62,12 +54,6 @@ function Timer() {
     window.location.href = "Timer.html"; // Redirecting to other page.
   }
 }
-// function update() {
-// 	var select = document.getElementById('input');
-// 	var option = select.options[select.selectedIndex];
-// 	document.getElementById('value').value = option.value;
-// }
-// update();
 
 //--------for add task field --------//
 function plus_icon() {
@@ -90,3 +76,9 @@ function hello() {
   window.location.href = "login.html"; // Redirecting to other page.
   return false;
 }
+
+// Task input get value
+// function getTaskVal() {
+//   var inputVal = document.getElementById("addTask").value;
+//   alert(inputVal);
+// }
